4ee26bddec36340baa55eef050f01b5829370b93
Nicholas Dronen <nicholas.dronen@here.com>
2018-12-17 15:32:24 -0700

    Snapshot of sigmoid.py

--- lectures/background/figures/sigmoid.py
+++ lectures/background/figures/sigmoid.py
@@ -11,9 +11,17 @@ def sigmoid(x):
 x = np.linspace(-5, 5, 20)
 y = sigmoid(x)
 
+plt.close('all')
+plt.figure()
 plt.plot(x, y)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Sigmoid function')
+plt.annotate('Small derivative',
+            xy=(4.5, 1), xytext=(2.5, 0.75),
+            arrowprops=dict(facecolor='black', shrink=0.05))
+plt.annotate('Small derivative',
+            xy=(-4.5, 0), xytext=(-5, 0.2),
+            arrowprops=dict(facecolor='black', shrink=0.05))
 plt.show(block=False)
 plt.savefig('sigmoid.pdf')
